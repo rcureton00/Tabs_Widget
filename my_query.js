83cff15999d857fd77368054cf868137ea5287c8
Reed Cureton <emailreedc@gmail.com>
2016-08-30 11:20:34 -0700

    Feat impliment each and isArray

--- my_query.js
+++ my_query.js
@@ -10,16 +10,47 @@
   };
 
   // Static Methods
-  var isArrayLike = function(obj) {
 
+  // Determine whether the argument is LIKE an array
+  // (numerical indicies & length property)
+  var isArrayLike = function(obj) {
+    if(typeof obj.length === 'number') {
+      if(obj.length === 0) {
+        return true;
+      } else if(obj.length > 0) {
+        return (obj.length - 1) in obj;
+      }
+    }
+    return false;
   };
 
   $.extend($, {
+    // Determine whether the argument is an array
     isArray: function(obj) {
+      return Object.prototype.toString.call(obj) === '[object Array]';
+    },
+
+    // Iterates over arrays or objects
+    each: function(collection, cb) {
+      if(isArrayLike(collection)) {
+        for(var i = 0; i < collection.length; i++) {
+            var val = collection[i];
+            cb.call(val, i, val);
+        }
+      } else {
+        for(var key in collection) {
+          if(collection.hasOwnProperty(key)) {
+            var val = collection[key]
+            cb.call(val, key, val);
+          }
+        }
+      }
+    },
+    makeArray: function(arr) {
 
     },
-    each: function(collection, cb) {
-      
+    proxy: function(fn, context) {
+
     }
   })
 
